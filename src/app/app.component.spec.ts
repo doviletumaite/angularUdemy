e00b5eac17e9735ffbf6e1a773d0c0071ab5161d
Dovile Tumaite <dovile.tumaite@yahoo.it>
2022-03-17 17:57:10 +0100

    up

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,7 +19,7 @@ describe('AppComponent', () => {
   it(`should have as title 'angularUdemy'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angularUdemy');
+    expect(app).toEqual('angularUdemy');
   });
 
   it('should render title', () => {
