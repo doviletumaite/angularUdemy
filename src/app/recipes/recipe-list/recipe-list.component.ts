cc11d6391b747cddf6bb99d52f068a4d42bce5e4
Dovile Tumaite <dovile.tumaite@yahoo.it>
2022-03-30 19:28:30 +0200

    up

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -8,14 +9,11 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>()
-  recipes: Recipe[] = [
-    new Recipe('test recepie 1', 'test', 'https://img.jamieoliver.com/jamieoliver/recipe-database/oldImages/large/1571_2_1437661403.jpg?tr=w-800,h-1066'),
-    new Recipe( 'test recepie 2', 'test', 'https://img.delicious.com.au/5aLcV7cG/del/2021/05/slow-roasted-butter-eggplant-curry-152139-2.jpg')
-    
-  ]
-  constructor() { }
+  recipes: Recipe[]
+  constructor(private recepieService: RecipeService) { }
 
   ngOnInit(): void {
+    this.recipes = this.recepieService.getRecipes()
   }
   onRecipeSelected(recipe: Recipe){
    this.recipeWasSelected.emit(recipe)
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,3 +1,11 @@
-export class RecipeService {
+import { Recipe } from "./recipe.model";
 
+export class RecipeService {
+  private recipes: Recipe[] = [
+    new Recipe('test recepie 1', 'test', 'https://img.jamieoliver.com/jamieoliver/recipe-database/oldImages/large/1571_2_1437661403.jpg?tr=w-800,h-1066'),
+    new Recipe( 'test recepie 2', 'test', 'https://img.delicious.com.au/5aLcV7cG/del/2021/05/slow-roasted-butter-eggplant-curry-152139-2.jpg')
+  ]
+  getRecipes(){
+    return this.recipes.slice()
+  }
 }
