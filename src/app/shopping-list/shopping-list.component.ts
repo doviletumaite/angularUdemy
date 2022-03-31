dff150f3ff4d47ad1f79b7e5aedd2c779c3b3b28
Dovile Tumaite <dovile.tumaite@yahoo.it>
2022-03-31 16:12:30 +0200

    servcs

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ingredient } from '../shared/ingredient.model';
+import { ShoppingService } from './shopping.service';
 
 @Component({
   selector: 'app-shopping-list',
@@ -7,14 +8,11 @@ import { ingredient } from '../shared/ingredient.model';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-  ingredients: ingredient[] = [
-    new ingredient("flour", 100),
-    new ingredient("tomatoes", 10),
-    new ingredient("chees", 60)
-  ]
-  constructor() { }
+  ingredients: ingredient[]
+  constructor(private shoppingService: ShoppingService) { }
 
   ngOnInit(): void {
+    this.ingredients = this.shoppingService.getIngredients()
   }
   onIngredientAdded(Ingredient: ingredient){
     this.ingredients.push(Ingredient)
