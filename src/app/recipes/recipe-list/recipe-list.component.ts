9b85598ad8bfdb7bc98eafbb55f174be6e7dd791
Dovile Tumaite <dovile.tumaite@yahoo.it>
2022-03-31 11:23:15 +0200

    services

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, OnInit,  } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 
@@ -8,14 +8,12 @@ import { RecipeService } from '../recipe.service';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  @Output() recipeWasSelected = new EventEmitter<Recipe>()
+
   recipes: Recipe[]
   constructor(private recepieService: RecipeService) { }
 
   ngOnInit(): void {
     this.recipes = this.recepieService.getRecipes()
   }
-  onRecipeSelected(recipe: Recipe){
-   this.recipeWasSelected.emit(recipe)
-  }
+
 }
