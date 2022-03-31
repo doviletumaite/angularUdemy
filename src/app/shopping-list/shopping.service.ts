88e16e2f67855d869c9f5d0a1de00a6973acaab3
Dovile Tumaite <dovile.tumaite@yahoo.it>
2022-03-31 15:58:20 +0200

    Update shopping.service.ts

--- src/app/shopping-list/shopping.service.ts
+++ src/app/shopping-list/shopping.service.ts
@@ -1,3 +1,12 @@
-export class ShoppingService {
+import { ingredient } from "../shared/ingredient.model";
 
+export class ShoppingService {
+  private ingredients: ingredient[] = [
+    new ingredient("flour", 100),
+    new ingredient("tomatoes", 10),
+    new ingredient("chees", 60)
+  ]
+  getIngredients(){
+    return this.ingredients.slice()
+  }
 }
