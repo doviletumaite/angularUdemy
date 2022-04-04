2eb0d61d33dcdea5d55f972737e8874ed82ad4fd
Dovile Tumaite <dovile.tumaite@yahoo.it>
2022-04-04 16:15:05 +0200

    Update recipe.service.ts

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -11,26 +11,20 @@ export class RecipeService {
       'Weird ratatuille',
       'gnummy',
       'https://img.jamieoliver.com/jamieoliver/recipe-database/oldImages/large/1571_2_1437661403.jpg?tr=w-800,h-1066',
-      [
-        new ingredient('potatoes', 2),
-        new ingredient('carrots', 3),
-      ]
+      [new ingredient('potatoes', 2), new ingredient('carrots', 3)]
     ),
     new Recipe(
       'Mess Eggplant',
       '10/10',
       'https://img.delicious.com.au/5aLcV7cG/del/2021/05/slow-roasted-butter-eggplant-curry-152139-2.jpg',
-      [
-        new ingredient('eggplant', 3),
-        new ingredient('cheese', 2),
-      ]
+      [new ingredient('eggplant', 3), new ingredient('cheese', 2)]
     ),
   ];
-  constructor(private shoppingService: ShoppingService){}
+  constructor(private shoppingService: ShoppingService) {}
   getRecipes() {
     return this.recipes.slice();
   }
-  addIngredientsToShoppingList(ingredients: ingredient[]){
-this.shoppingService
+  addIngredientsToShoppingList(ingredients: ingredient[]) {
+    this.shoppingService.addIngredients(ingredients);
   }
 }
