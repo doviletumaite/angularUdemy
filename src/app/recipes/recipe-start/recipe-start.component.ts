a6d4835a4a69f9c04824fe17c40405aa15382f51
Dovile Tumaite <dovile.tumaite@yahoo.it>
2022-04-06 12:31:23 +0200

    up

---
+++ src/app/recipes/recipe-start/recipe-start.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-start',
+  templateUrl: './recipe-start.component.html',
+  styleUrls: [],
+})
+export class RecipeStartComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
