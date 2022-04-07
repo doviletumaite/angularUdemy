09d384c8b4800eeeace96ed69d57ce43e6320248
Dovile Tumaite <dovile.tumaite@yahoo.it>
2022-04-07 12:28:56 +0200

    up

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -28,4 +28,5 @@ export class RecipeDetailComponent implements OnInit {
   onAddToShoppingList() {
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
   }
+  onEditRecipe() {}
 }
